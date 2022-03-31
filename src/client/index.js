1a7efeef0a2370efc4bcfc5e198c512efc4db231
Sinai Alfaro <97054941+SinaiAlH@users.noreply.github.com>
2022-03-31 12:49:13 -0600

    Ready

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,12 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,7 @@
+//
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -15,9 +19,15 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!');
+    console.log(`Your API key is ${process.env.API_KEY}`);
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+//API credential statement
+var textapi =new aylien({
+    application_key: process.env.API_KEY
+  });
